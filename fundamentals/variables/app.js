966a451bd35f36790358a57c298365ada1f4558d
ahmed-razic <ahmed.razic@gmail.com>
2022-04-08 23:47:30 +0200

    variables

---
+++ fundamentals/variables/app.js
@@ -0,0 +1,41 @@
+//var
+
+var firstName = 'Ahmed';
+console.log(firstName);
+
+//let
+
+let lastName = 'Razic';
+console.log(lastName);
+
+//const
+
+const PI = 3.1415;
+//PI = 2;
+
+const arr = [1, 2, 3, 4, 5];
+
+//arr = [1, 2, 3, 4, 5];
+console.log(arr);
+arr.pop();
+arr.pop();
+console.log(arr);
+arr.push(5);
+console.log(arr);
+arr.unshift(0);
+console.log(arr);
+arr.shift();
+console.log(arr);
+
+const person = {
+  name: 'Ahmed',
+  age: 46,
+  employed: true,
+  city: 'Sarajevo',
+};
+
+console.log(person);
+person.married = true;
+console.log(person);
+
+//person = 'Ahmed';
