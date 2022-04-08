74dde0828a3b500223ab05b65bef9f0af523edfc
ahmed-razic <ahmed.razic@gmail.com>
2022-04-08 23:32:17 +0200

    initial commit

---
+++ fundamentals/chrome-console/app.js
@@ -0,0 +1,35 @@
+const str = 'Hello',
+  num = 2,
+  bool = true,
+  arr = [1, 2, 3],
+  person = { a: 1, b: 2, c: 3 };
+
+console.log(str);
+console.log(num);
+console.log(arr);
+console.log(person);
+console.log(person.a);
+console.log(person.b);
+console.table(person);
+//console.table({ a: 1, b: 2, c: 3 });
+
+console.error('An error...');
+console.warn('A warning...');
+//console.clear();
+//console.count();
+console.timeStamp('start');
+console.time('start');
+console.log('time');
+console.log('time');
+console.log('time');
+console.log('time');
+console.log('time');
+console.log('time');
+console.log('time');
+console.log('time');
+console.log('time');
+console.log('time');
+console.log('time');
+console.log('time');
+console.log('time');
+console.timeEnd('start');
