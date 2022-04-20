a6625b8037b395d5fd7ed8db0ae9d0abfdc26c70
ahmed-razic <ahmed.razic@gmail.com>
2022-04-20 23:11:56 +0200

    fetch-promise

--- 05-asynchronous-JS/10-examples/fetch-promises/app.js
+++ 05-asynchronous-JS/10-examples/fetch-promises/app.js
@@ -2,8 +2,22 @@ const http = new HTTP();
 
 //http.get('https://api.github.com/users').then((data) => console.log(data));
 
+// http
+//   .post('https://jsonplaceholder.typicode.com/posts', {
+//     id: 1000000000,
+//     login: 'ahmed',
+//     type: 'begginer',
+//   })
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error));
+
+// http
+//   .delete('https://jsonplaceholder.typicode.com/posts/10')
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
 http
-  .post('https://jsonplaceholder.typicode.com/posts', {
+  .put('https://jsonplaceholder.typicode.com/posts/10', {
     id: 1000000000,
     login: 'ahmed',
     type: 'begginer',
--- 05-asynchronous-JS/10-examples/fetch-promises/http.js
+++ 05-asynchronous-JS/10-examples/fetch-promises/http.js
@@ -20,4 +20,29 @@ class HTTP {
         .catch((error) => reject(error));
     });
   }
+
+  delete(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'DELETE',
+        headers: { 'Content-type': 'application/json' },
+      })
+        .then((response) => response.json())
+        .then(() => resolve('deleted'))
+        .catch((err) => reject(err));
+    });
+  }
+
+  put(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'PUT',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+  }
 }
