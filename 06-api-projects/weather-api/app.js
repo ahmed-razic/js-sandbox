2317ec5d86f182650b31011a4796d9204e647a65
ahmed-razic <ahmed.razic@gmail.com>
2022-04-21 23:06:46 +0200

    change location

--- 06-api-projects/weather-api/app.js
+++ 06-api-projects/weather-api/app.js
@@ -1,6 +1,17 @@
 const weather = new Weather('Mekka', 'SA');
 const ui = new UI();
 document.addEventListener('DOMContentLoaded', getWeather);
+document.getElementById('save-changes').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
+
+  weather.changeLocation(city, state);
+
+  getWeather();
+
+  //use jquery to close modal
+  $('#locationModal').modal('hide');
+});
 
 function getWeather() {
   weather
