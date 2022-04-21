9eb0ebc1808c3de7bf83ec748152fa4795994628
ahmed-razic <ahmed.razic@gmail.com>
2022-04-21 23:34:16 +0200

    set local storage

--- 06-api-projects/weather-api/app.js
+++ 06-api-projects/weather-api/app.js
@@ -1,12 +1,19 @@
-const weather = new Weather('Mekka', 'SA');
 const ui = new UI();
+const storage = new Storage();
+const locationData = storage.getLocationData();
+
+const weather = new Weather(locationData.city, locationData.state);
+
 document.addEventListener('DOMContentLoaded', getWeather);
 document.getElementById('save-changes').addEventListener('click', (e) => {
   const city = document.getElementById('city').value;
   const state = document.getElementById('state').value;
 
   weather.changeLocation(city, state);
+  storage.setLocationData(city, state);
 
+  console.log(city, state);
+  console.log(storage.getLocationData());
   getWeather();
 
   //use jquery to close modal
