180ed289a80d69ec4dc3b0007ae296956d2631a2
ahmed-razic <ahmed.razic@gmail.com>
2022-04-23 19:20:50 +0200

    finished profile scroller with iterator function

--- 11-profile-scroller-app/app.js
+++ 11-profile-scroller-app/app.js
@@ -24,3 +24,41 @@ const data = [
     image: 'https://randomuser.me/api/portraits/men/15.jpg',
   },
 ];
+
+const profiles = profileIterator(data);
+showProfile();
+
+function profileIterator(profiles) {
+  let i = 0;
+
+  return {
+    next: function () {
+      return i < profiles.length
+        ? { value: profiles[i++], done: false }
+        : { done: true };
+    },
+  };
+}
+
+document.getElementById('next').addEventListener('click', showProfile);
+
+function showProfile() {
+  const currentProfile = profiles.next().value;
+
+  if (currentProfile !== undefined) {
+    document.getElementById(
+      'imageDisplay'
+    ).innerHTML = `<img src="${currentProfile.image}"/>`;
+
+    document.getElementById('profileDisplay').innerHTML = `
+    <ul class="list-group">
+      <li class="list-group-item">Name: ${currentProfile.name}</li>
+      <li class="list-group-item">Gender: ${currentProfile.gender}</li>
+      <li class="list-group-item">Age: ${currentProfile.age}</li>
+      <li class="list-group-item">Location: ${currentProfile.location}</li>
+    </ul>
+    `;
+  } else {
+    window.location.reload();
+  }
+}
